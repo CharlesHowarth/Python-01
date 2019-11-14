0a2bfb8942388290ce807ae4777dc417fe184bb0
CharlesHowarth <57762027+CharlesHowarth@users.noreply.github.com>
2019-11-14 15:29:21 +0000

    days 1-3 code

---
+++ dot_methods.py
@@ -0,0 +1,14 @@
+test_str = "abcdef"
+print(test_str.capitalize())
+
+test_str = "abcdef"
+print(test_str.upper())
+
+test_str = "abcdef"
+print(test_str.count("bc"))
+
+test_str = "abcdef"
+print(test_str.find("d"))
+
+test_str = "abcdef"
+print(test_str.replace("bc","de"))
---
+++ function_return.py
@@ -0,0 +1,20 @@
+def test(str_1):
+    return str_1 + " test"
+
+print(test("this is a"))
+
+
+
+def test_1(int_1):
+    return int_1 * 2
+
+print("10 * 2 = " + str(test_1(10)))
+
+
+
+
+def test_2(int_2):
+    return int_2 > 100
+
+print("201 is greater than 100 is " + str(test_2(201)))
+
