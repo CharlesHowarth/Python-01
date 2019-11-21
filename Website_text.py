606357966bda6037f3d5d736b08451cda6e9f3fb
CharlesHowarth <57762027+CharlesHowarth@users.noreply.github.com>
2019-11-21 12:24:03 +0000

    Second version

--- Website_text.py
+++ Website_text.py
@@ -10,11 +10,16 @@ the course.
 
 
 
+
+
 Purpose of IT in modern business.
 The use of Information Technology within modern businesses has been viewed as an essential for decades. For
 any business to be competitive they must embrace IT. The uses of IT include improving communication,
 automating time consuming processes, and providing security to keep proprietary data safe. Good It implementation
 will also lead to better employee satisfaction, moral and efficiency.
+
+
+
 
     
 
@@ -44,3 +49,52 @@ https://www.howtogeek.com/428046/what-is-slack-and-why-do-people-love-it/
 
 
 
+Planning and carrying out tasks:
+Trello will be the choice of tool for creating the website. It will be used to break down tasks into individual
+components, and these will be tracked through the stages of development (to do, doing, text version created, incorporated into website).
+
+https://trello.com/b/IxsMD2UX/website-project
+
+
+
+
+Risks in Digital Projects:
+There are various reasons behind digital projects failing. A recent survey revealed that digital risk is a very widespread problem with
+97% of compainies surveyed reporting that they had experienced digital project failure. Digital failure is strongly linked to the companies
+overall performance.
+
+A breakdown of reasons given for failure states that:-
+51% - short term priorities took precedence leading to project slip
+46% - unclear vision
+51% - poor scope definition 
+50% - trying to do too much at once.
+39.5% - poor communication/siloing between IT and marketing
+
+https://www.digitaldoughnut.com/articles/2018/september/why-digital-projects-fail
+
+
+
+
+
+Technology selection:
+where there is an industry standard in place, then it is only sensible to adopt that standard. For example, Slack and Trello are widely used
+and have proven track records so it is desirable to incorporate these tools in digital projects. Where there is less clarity, i.e. there is more
+than one dependable solution, then several considerations are factored in.
+
+1) Ease Of use, Beginner friendly
+2) Versatility
+3) Performance
+4) Scalability
+5) Growth opportunity for developers
+6) Tech trends
+
+When the relative merits of one language over another are assessed within these categories, it should be easier to determine which language
+is more suitable for your project.
+
+https://codeburst.io/python-vs-javascript-battle-for-supremacy-ba42a76912f6
+
+
+
+Trello GitHub assessments
+Trello has been a very useful tool for breaking down projects into manageable units. I have used it for both the Python RPG project and to
+manage the necessary items for building the website for the CodeNation course. Github is very useful 
