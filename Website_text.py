eacee3d9b859abea0c7cf2434510f49ed272f8fb
CharlesHowarth <57762027+CharlesHowarth@users.noreply.github.com>
2019-11-21 11:35:16 +0000

    Add files via upload

---
+++ Website_text.py
@@ -0,0 +1,46 @@
+
+"""
+
+About Me:
+I have a background in IT that stretches back to 1984. I have worked extensively with relational databases
+and have worked for big name firms such as Unisys. I had to take some time away from the world of IT for
+family reasons, but recently decided to use Code Nation to help me refresh my skills for the modern
+workplace. I have learnt some Python basics which I have put to use successfully within
+the course.
+
+
+
+Purpose of IT in modern business.
+The use of Information Technology within modern businesses has been viewed as an essential for decades. For
+any business to be competitive they must embrace IT. The uses of IT include improving communication,
+automating time consuming processes, and providing security to keep proprietary data safe. Good It implementation
+will also lead to better employee satisfaction, moral and efficiency.
+
+    
+
+Methods, Skills and Resources
+
+IT Project management is essential to deliver successful projects. Trello is a very useful tool to plan
+and monitor projects throughout their lifecycle. It assists with visibilty for all memebers of a project team.
+Effecfiency, productivity and stability are improved by incorporating an industry standard structured approach
+to development. Team capacity and customer demand can be managed and aligned more quickly with tools such as Trello. 
+https://blog.trello.com/kanban-data-nave
+
+Relevant development skills will be required to make projects succeed.
+According to Forbes, the most in demand languages are:-
+1) Python
+2) Java
+3) Javascript
+4) c#
+5) PHP
+https://www.forbes.com/sites/jeffkauflin/2017/05/12/the-five-most-in-demand-coding-languages/#49a088fdb3f5
+
+Slack is the communication tool of preference for the IT industry.
+Slack relies on channels and DM's as the two main methods of communications. Channels allow posts to be hashtagged
+so that they are organised in a way that allows users to filter in relevant posts. DM's or Direct Messages allow for
+users to connect on an individual basis
+https://www.howtogeek.com/428046/what-is-slack-and-why-do-people-love-it/
+
+
+
+
